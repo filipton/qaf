278d1c755e610be74fbdc5375346e88ece5c8ee1
filipton <filipton12@gmail.com>
2023-03-18 23:33:41 +0100

    feat: actix_scope file

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer};
+mod actix_scope;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(web::scope("")
-.service(web::scope("inner")
-.service(pages::inner::test2::get_test2_index)
-.service(web::scope("nested")
-.service(pages::inner::nested::siema::get_kurwa_index)
-)).service(pages::test::get_test_index)
-.service(pages::test::get_post_index)
-.service(web::scope("outer")
-.service(pages::outer::si::get_test2_index)
-).service(pages::another::get_test2_index)
-)
-    )
+    HttpServer::new(|| App::new().service(actix_scope::generated_scope()))
         .bind(("0.0.0.0", 8081))?
         .run()
         .await
 }
-
-pub mod pages { 
-	pub mod inner { 
-		pub mod test2; 
-		pub mod nested { 
-			pub mod siema; 
-		} 
-	} 
-	pub mod test; 
-	pub mod outer { 
-		pub mod si; 
-	} 
-	pub mod another; 
-} 
-
